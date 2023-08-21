796f8659a784ebdc5e52b75101b362332ba78f07
Taewon Hwang <htw1203@gmail.com>
2023-08-21 11:05:21 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/footballcinema/FootballCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.footballcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/footballcinema/FootballCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.footballcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
